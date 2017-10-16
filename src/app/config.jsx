2cd1c922d4e888a29007c4b592a7db2c1cf9b907
pkpgdjk <ronnachai45399@gmail.com>
2017-10-16 11:55:57 +0700

    build-in api server

--- src/app/config.jsx
+++ src/app/config.jsx
@@ -1,5 +1,6 @@
 export const ENV = {
-    API_URL : "http://localhost/",
+    // API_URL : "http://localhost/",
+    API_URL : "http://localhost:8000/",
     // API_URL : "http://teamport.epizy.com/",
 };
 
